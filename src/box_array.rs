fe5fd114163804f7deb3c526a80ca9c86e067560
Lionel Flandrin <lionel.flandrin@gmail.com>
2016-03-07 22:09:33 +0100

    Minor cleanup in box_array

--- src/box_array.rs
+++ src/box_array.rs
@@ -9,13 +9,14 @@
 ///     let _: Box<[u8; 1024]> = box_array![0; 1024];
 /// ```
 macro_rules! box_array {
-    ( $val: expr ; $len: expr ) => ({
+    ($val:expr ; $len:expr) => ({
         // Use a generic function so that the pointer cast remains
         // type-safe
         fn vec_to_boxed_array<T>(vec: Vec<T>) -> Box<[T; $len]> {
             let boxed_slice = vec.into_boxed_slice();
 
-            let ptr = ::std::boxed::Box::into_raw(boxed_slice) as *mut [T; $len];
+            let ptr =
+                ::std::boxed::Box::into_raw(boxed_slice) as *mut [T; $len];
 
             unsafe { Box::from_raw(ptr) }
         }
