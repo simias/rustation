5b35772be62eeb1e0626f9ec7b596238a6627748
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-21 03:30:53 +0000

    Pending changes exported from your codespace

--- src/memory/bios.rs
+++ src/memory/bios.rs
@@ -43,6 +43,20 @@ impl Bios {
     pub fn load8(&self, offset: u32) -> u8 {
         self.data[offset as usize]
     }
+
+    pub fn store32(&mut self, offset: u32, val: u32) {
+        let offset = offset as usize;
+
+        let b0 = val as u8;
+        let b1 = (val >> 8) as u8;
+        let b2 = (val >> 16) as u8;
+        let b3 = (val >> 24) as u8;
+
+        self.data[offset + 0] = b0;
+        self.data[offset + 1] = b1;
+        self.data[offset + 2] = b2;
+        self.data[offset + 3] = b3;
+    }
 }
 
 /// BIOS images are always 512KB in length
